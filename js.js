e998f1cdffd5a115e8c38483741f49c6fcc24777
TaylorEisman12 <taylor@theeismans.com>
2012-04-05 09:18:16 -0500

    Started my Js file, added my getElement function

--- js.js
+++ js.js
@@ -1,1 +1,13 @@
 // Taylor Eisman
+// VFW 03/12
+// Project 2
+
+window.addEventListener("DOMContentLoaded", function({
+
+  function $(x){
+		var theElement = document.getElementById(x);
+		return theElement;
+	}
+	
+
+});
