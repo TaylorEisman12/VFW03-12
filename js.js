ac14c5e22cfc058910759eb7c7875c1f44743609
TaylorEisman12 <taylor@theeismans.com>
2012-04-05 11:03:22 -0500

    Started my makeCats function and for loop to populate the drop down selections

--- js.js
+++ js.js
@@ -9,6 +9,20 @@ window.addEventListener("DOMContentLoaded", function({
 		return theElement;
 	}
 	
+	function makeCats(){
+		var formTag = document.getElementsByTagName("form"),
+			selectLi = $('select'),
+			makeSelect = document.createElement('select');
+			makeSelect.setAttribute("id", "category");
+			
+		for (var i=0, j=categories.length; i<j; i++){
+			var makeOption = document.createElement('option');
+			var optText = categories[i];
+			makeOption.setAttribute("value", optText);
+			makeOption.innerHTML = optText;
+		}
+	}
+
 	var category = ["-- Category --", "Cell Phone", "Car", "Rent", "Cable"];
 		
 	var displayLink = $('displayLink');	
