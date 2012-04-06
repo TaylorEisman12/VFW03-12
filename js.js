0b5c90ae6b2c6299b7b510fb5eb067564c9d607a
TaylorEisman12 <taylor@theeismans.com>
2012-04-05 19:01:25 -0500

    Created my storeData function

--- js.js
+++ js.js
@@ -2,9 +2,9 @@
 // VFW 03/12
 // Project 2
 
-window.addEventListener("DOMContentLoaded", function({
+window.addEventListener("DOMContentLoaded", function(){
 
-  function $(x){
+	function $(x){
 		var theElement = document.getElementById(x);
 		return theElement;
 	}
@@ -20,11 +20,34 @@ window.addEventListener("DOMContentLoaded", function({
 			var optText = categories[i];
 			makeOption.setAttribute("value", optText);
 			makeOption.innerHTML = optText;
+			makeSelect.appendChild(makeOption);
 		}
+		selectLi.appendChild(makeSelect);
 	}
-
-	var category = ["-- Category --", "Cell Phone", "Car", "Rent", "Cable"];
+	
+	function storeData(){
+		var id			= Math.floor(Math.random()*100000001);
 		
+		var item				= ();
+			item.category			= ["Category: ", $('category').value];
+			item.compName			= ["Company Name: ", $('compName').value];
+			item.compEmail			= ["Company Email: ", $('compEmail').value];
+			item.compWeb			= ["Company Website: ", $('compWeb').value];
+			item.payBy				= ["Pay By: ", payByValue];
+			item.pastDue			= ["Past Due: ", pastDueValue];
+			item.budgetPercent		= ["Percent of Budget: ", $('budgetPercent').value];
+			item.date				= ["Date Added: ", $('date').value];
+			item.notes				= ["Notes: ", $('notes').value];
+		
+		localStorage.setItem(id, JSON.stringify(item));
+		alert("Bill Saved.");
+	
+	}
+	
+	var categories = ["-- Category --", "Cell Phone", "Car", "Rent", "Cable"];
+	
+	makeCats();
+	
 	var displayLink = $('displayLink');	
 	displayLink.addEventListener("click", getData);
 	var clearLink = $('clear');
