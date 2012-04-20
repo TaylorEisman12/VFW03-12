d26788fd22c47198b7ee71083a0d275dd45f992a
TaylorEisman12 <taylor@theeismans.com>
2012-04-19 20:59:50 -0500

    Changed getData function

--- js.js
+++ js.js
@@ -94,24 +94,26 @@ window.addEventListener("DOMContentLoaded", function(){
 		var makeList = document.createElement('ul');
 		makeDiv.appendChild(makeList);
 		document.body.appendChild(makeDiv);
-		$('items').stlye.display = "display";
-		for(var i=0, len=localStoarge.length; i<len;i++){
+		$('items').style.display = "display";
+		for(var i=0, len=localStorage.length; i<len;i++){
 			var makeli = document.createElement('li');
+			var linksLi = document.createElement('li');
 			makeList.appendChild(makeli);
 			var key = localStorage.key(i);
 			var value = localStorage.getItem(key);
 			var obj = JSON.parse(value);
 			var makeSubList = document.createElement('ul');
-			makeLi.appendChild(makeSubList);
+			makeli.appendChild(makeSubList);
 			for(var n in obj){
 				var makeSubli = document.createElement('li');
 				makeSubList.appendChild(makeSubli);
 				var optSubText = obj[n][0]+" "+obj[n][1];
 				makeSubli.innerHTML = optSubText;
+				makeSubList.appendChild(linksLi);
 			}
+			makeItemLinks(localStorage.key(i), linksLi);
 		}
 	}
-	
 	function clearLocal(){
 		if(localStorage.length === 0){
 			alert("No data to clear.");
